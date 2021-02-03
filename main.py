03e39b66b71d2dfef1d223dac05971084f5ef5fa
JSWeeks <paintingwithjason@gmail.com>
2021-02-03 11:31:33 -0500

    reloaded file

--- main.py
+++ main.py
@@ -18,7 +18,6 @@ LOG_LEVEL = os.environ.get('LOG_LEVEL', 'INFO')
 def _logger():
     '''
     Setup logger format, level, and handler.
-
     RETURNS: log object
     '''
     formatter = logging.Formatter(
